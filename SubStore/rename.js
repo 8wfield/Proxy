56d6af2aee0f640fad66a4132b17ede7d8536ace
8w <ttttw1118@163.com>
2026-01-06 23:34:11 +0800

    更新 rename.js

--- SubStore/rename.js
+++ SubStore/rename.js
@@ -43,7 +43,7 @@ const nx = inArg.nx || false,
   key = inArg.key || false,
   blgd = inArg.blgd || false,
   blnx = inArg.blnx || false,
-  clear = true,
+  clear = inArg.clear || false
   addflag = inArg.flag || false,
   nm = inArg.nm || false;
 
